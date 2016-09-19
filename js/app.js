66c8561848b073ce1f530e39eda51fc4a8fcbb33
MJ gululu <chenmengchieh2012@gmail.com>
2016-09-19 17:43:45 +0800

    index

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+
+$( document ).ready(function() {
+	var util = new Util();
+    $('.menu-card').click(function(){
+    	var template = util.get_template_byID($(this).attr('id'));
+    	console.log($(this).attr('id')+','+template);
+    	$('#context').load(template);
+    });
+});
---
+++ js/util.js
@@ -0,0 +1,41 @@
+function Util(){
+
+}
+
+var TEMPLATE = [
+	{
+		_id : "main",
+		template : "./templates/home.html"
+	},
+	{
+		_id : "calander",
+		template : "./templates/calander.html"
+	},
+	{
+		_id : "timelime",
+		template : "./templates/timelime.html"
+	},
+	{
+		_id : "post",
+		template : "./templates/post.html"
+	},
+	{
+		_id : "profile",
+		template : "./templates/profile.html"
+	},
+	{
+		_id : "about",
+		template : "./templates/about.html"
+	}
+
+]
+
+
+Util.prototype.get_template_byID = function(id){
+	for(var i=0;i<TEMPLATE.length;i++){
+		if(TEMPLATE[i]['_id'] == id){
+			return TEMPLATE[i]['template'];
+		}
+	}
+	return null;
+}
