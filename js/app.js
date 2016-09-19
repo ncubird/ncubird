354d98a24196a47779983111cf3b22e6a57f7ce1
MJ gululu <chenmengchieh2012@gmail.com>
2016-09-19 17:48:13 +0800

    index

--- js/app.js
+++ js/app.js
@@ -5,6 +5,6 @@ $( document ).ready(function() {
     $('.menu-card').click(function(){
     	var template = util.get_template_byID($(this).attr('id'));
     	console.log($(this).attr('id')+','+template);
-    	$('#context').load(template);
+    	$('.context').load(template);
     });
 });
