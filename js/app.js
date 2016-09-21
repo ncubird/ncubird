420130b2be372ebf4e642fabccd26b3e1b7bc657
ncubird <ncubird123@gmail.com>
2016-09-21 21:27:29 +0800

    test

--- js/app.js
+++ js/app.js
@@ -17,9 +17,9 @@ $( document ).ready(function() {
                     canalnder_contoller.set_today_and_sync();
                     canalnder_contoller.generate_offset();
                     canalnder_contoller.set_calander_template(canalnder_contoller.year,canalnder_contoller.month,function(){
-                        $("."+root_background).css('height','0xp');
-                        $("."+root_background).height(0);
-                        $("."+root_background).css('height',($( document ).height()-$( '.logo-bird' ).height())+'px');
+                        $(".root-background").css('height','0xp');
+                        $(".root-background").height(0);
+                        $(".root-background").css('height',($( document ).height()-$( '.logo-bird' ).height())+'px');
                         canalnder_contoller.set_calander_tag(2016,8,20,100000244681661,"test");
                     });
                     console.log("calander");
