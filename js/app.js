7a4a405a7ce93c6f103d48d0da51feeb4f9bdf8e
ncubird <ncubird123@gmail.com>
2016-09-22 23:34:00 +0800

    test

--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@
 $( document ).ready(function() {
 
     console.log("ready");
-    var Facebook_sdk = new Fb_sdk();
-    Facebook_sdk.init();  
 
 	var util = new Util();
     var calendar_controller = new Calander_controller('calander-days','calander-slider-month','calander-slider-year');
