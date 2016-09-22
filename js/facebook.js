706c5ca29af46dba480bd0516138061494e03ad8
ncubird <ncubird123@gmail.com>
2016-09-23 01:00:05 +0800

    test

--- js/facebook.js
+++ js/facebook.js
@@ -5,8 +5,9 @@ function Fb_sdk(){
 Fb_sdk.prototype.send_message = function(facebook_id){
   FB.ui({
         method:'send',
-        app_id:'177786062658873',
-        to:facebook_id
+        app_id:'177786062658873', 
+        name: 'Send Private Message',
+        to:[facebook_id]
     });
 }
 
