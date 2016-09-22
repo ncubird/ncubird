5b2e936269fa7c95976e05d59237265c49bceb66
ncubird <ncubird123@gmail.com>
2016-09-23 00:25:39 +0800

    test

--- js/app.js
+++ js/app.js
@@ -4,10 +4,12 @@
 $( document ).ready(function() {
 
     console.log("ready");
+    var fb_sdk = new Fb_sdk();
 
 	var util = new Util();
     var calendar_controller = new Calander_controller('calander-days','calander-slider-month','calander-slider-year');
     var post_controller = new Post_controller('post_button');
+    var about_controller = new About_controller('post_button');
     var module_google_script = new Module_google_script();
     $('.menu-card').click(function(){
     	var template = util.get_template_byID($(this).attr('id'));
@@ -30,16 +32,23 @@ $( document ).ready(function() {
                     console.log("calander");
                     calendar_controller.set_calander_tag(2016,8,20,100000244681661,"test");
                     
-                }
-                break;
+                    }
+                    break;
 
                 case 'post':{
                     post_controller.init(function(send_data){
                         console.log(JSON.stringify(send_data));
                         module_google_script.event_add(send_data);
                     });
-                }
-                break;
+                    }
+                    break;
+
+                case 'about':{
+                        about_controller.test(function(){
+                            fb_sdk.send_message($('#facebook_userid'));
+                        })                        
+                    }
+                    break;
             }
 
             $(".root-background").css('height',($( document ).height()-$( '.logo-bird' ).height())+'px');
--- js/facebook.js
+++ js/facebook.js
@@ -2,6 +2,13 @@ function Fb_sdk(){
 
 }
 
+Fb_sdk.prototype.send_message = function(facebook_id){
+  FB.ui({
+        app_id:'177786062658873',
+        to:facebook_id
+    });
+}
+
 
 
   
