a570136aec924acd17db7a8ba94a3b6315f75e53
ncubird <ncubird123@gmail.com>
2016-09-22 22:59:24 +0800

    test

--- js/facebook.js
+++ js/facebook.js
@@ -45,13 +45,11 @@ Fb_sdk.prototype.statusChangeCallback = function(response) {
       // Logged into your app and Facebook.
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
+      $('.cssroot').html('Please log into this app.');
     } else {
       // The person is not logged into Facebook, so we're not sure if
       // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
+      $('.cssroot').html('Please log into Facebook.');
     }
 }
 
