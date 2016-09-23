10b7f96ac7a0b1bad3514d568798758a2bbe5a7c
ncubird <ncubird123@gmail.com>
2016-09-24 01:33:45 +0800

    test

--- js/calander.js
+++ js/calander.js
@@ -47,12 +47,12 @@ Calander_controller.prototype.set_calander_template = function (year,month,event
 			$('.'+this.calander_class).html($('.'+this.calander_class).html() + this.template_calander_classlebox());
 		}
 
-		for(var i=1;i<=MONTH_DAYS[month];i++){
+		for(var i=0;i<MONTH_DAYS[month];i++){
 			$('.'+this.calander_class).html($('.'+this.calander_class).html() + this.template_calander_box(year,month,i));
 		}
 
 		if(month == 1 && this.is_spectial_Feb(year)){
-			$('.'+this.calander_class).html($('.'+this.calander_class).html() + this.template_calander_box(year,month,29));
+			$('.'+this.calander_class).html($('.'+this.calander_class).html() + this.template_calander_box(year,month,28));
 		}
 	}
 
