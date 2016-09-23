a0db87b5feb54f4afc8c53d0e7f86c1e1cb38080
ncubird <ncubird123@gmail.com>
2016-09-24 03:48:01 +0800

    test

--- js/calander.js
+++ js/calander.js
@@ -90,14 +90,14 @@ Calander_controller.prototype.set_click_event = function(event_callback){
 
 	$("#calander_search_type").off('change')
 	$("#calander_search_type").on('change',function(){
-		this.search_type = $("#calander_search_type").val();
-		self.calander_refresh_tag(this.tmp_data);
+		self.search_type = $("#calander_search_type").val();
+		self.calander_refresh_tag(self.tmp_data);
 	})
 
 	$("#calander_search_info").off('change')
 	$("#calander_search_info").on('change',function(){
-		this.search_info = $("#calander_search_info").val();
-		self.calander_refresh_tag(this.tmp_data);
+		self.search_info = $("#calander_search_info").val();
+		self.calander_refresh_tag(self.tmp_data);
 	})
 
 	$('.'+this.title_year_class).html(this.year);
