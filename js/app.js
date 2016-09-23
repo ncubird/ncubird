32846682d37eafe449be666b937fce03f39612fc
ncubird <ncubird123@gmail.com>
2016-09-24 01:09:54 +0800

    test

--- js/app.js
+++ js/app.js
@@ -66,7 +66,7 @@ $( document ).ready(function() {
 });
 
 function calander_add_tag(res){
-    for(var i;i<res.length;i++){
+    for(var i=0;i<res.length;i++){
         var start_time = new Date(res['start']);
         var end_time = new Date(res['end']);
         var other_message = res['other_message'];                            
