d14503fe32b03463a3a42c9fb582e718bf286d28
ncubird <ncubird123@gmail.com>
2016-09-24 19:53:17 +0800

    test

--- js/information_block.js
+++ js/information_block.js
@@ -46,7 +46,7 @@ Information_block.prototype.show_block = function(data,facebook_id,post_time,cal
 
 				var time = new Date(data[i][INFO_UTIL.ROOT_DATA_KEY.START]);
 				var send_starttime = new Date(time.getYear(),time.getMonth(),1,0,0,0);
-				var send_endtime = new Data(time.getYear(),time.getMonth(),((self.is_spectial_Feb(time.getYear()) && time.getMonth() == 1)? 29 : INFO_MONTH_DAYS[time.getMonth()]),23,59,00);
+				var send_endtime = new Date(time.getYear(),time.getMonth(),((self.is_spectial_Feb(time.getYear()) && time.getMonth() == 1)? 29 : INFO_MONTH_DAYS[time.getMonth()]),23,59,00);
 
 				send_data[INFO_UTIL.ROOT_DATA_KEY.START] = send_starttime.getTime();
 				send_data[INFO_UTIL.ROOT_DATA_KEY.END] = send_endtime.getTime();
