4d3ca27954a8f54ab3da8f81f6b199ec14169862
ncubird <ncubird123@gmail.com>
2016-09-26 00:16:39 +0800

    test

--- js/app.js
+++ js/app.js
@@ -300,6 +300,32 @@ $( document ).ready(function() {
 
 });
 
+var observeDOM = (function(){
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
+        eventListenerSupported = window.addEventListener;
+
+    return function(obj, callback){
+        if( MutationObserver ){
+            // define a new observer
+            var obs = new MutationObserver(function(mutations, observer){
+                if( mutations[0].addedNodes.length || mutations[0].removedNodes.length )
+                    callback();
+            });
+            // have the observer observe foo for changes in children
+            obs.observe( obj, { childList:true, subtree:true });
+        }
+        else if( eventListenerSupported ){
+            obj.addEventListener('DOMNodeInserted', callback, false);
+            obj.addEventListener('DOMNodeRemoved', callback, false);
+        }
+    }
+})();
+
+// Observe a specific DOM element:
+observeDOM( document.getElementById('dom_element') ,function(){ 
+    console.log('dom changed');
+});
+
 
 
 
