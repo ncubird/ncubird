2153df47a3b8545ff1f16c200c5c7b8ac224f07f
ncubird <ncubird123@gmail.com>
2016-09-26 04:16:35 +0800

    test

--- js/facebook.js
+++ js/facebook.js
@@ -3,13 +3,24 @@ function Fb_sdk(){
 }
 
 Fb_sdk.prototype.send_message = function(facebook_id){
-  FB.ui({
-        method:'send',
-        app_id:'177786062658873',
-        link:'https://ncubird.github.io/ncubird/',
-        name: 'Send Private Message',
-        to:facebook_id
-    });
+  // FB.ui({
+  //       method:'send',
+  //       app_id:'177786062658873',
+  //       link:'https://ncubird.github.io/ncubird/',
+  //       name: 'Send Private Message',
+  //       to:facebook_id
+  //   });
+
+  if (navigator.userAgent.indexOf("Mobi") > -1){
+      window.location.assign("#page-2");
+  } else {
+      window.location.replace("https://www.facebook.com/dialog/send?" + 
+                              "app_id=123050457758183" + 
+                              "&link=https://ncubird.github.io/ncubird/" + 
+                              "&redirect_uri=https://ncubird.github.io/ncubird/"+
+                              "&to="+JSON.stringify(facebook_id);
+      );
+  }
 }
 
 
