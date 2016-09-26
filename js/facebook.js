2524ed968bccccbec243ad33ac51a55b33900b7c
ncubird <ncubird123@gmail.com>
2016-09-26 22:03:33 +0800

    test

--- js/facebook.js
+++ js/facebook.js
@@ -3,25 +3,25 @@ function Fb_sdk(){
 }
 
 Fb_sdk.prototype.send_message = function(facebook_id){
-  FB.ui({
-        method:'send',
-        app_id:'177786062658873',
-        link:'https://ncubird.github.io/ncubird/',
-        name: 'Send Private Message',
-        to:facebook_id
-    });
+  // FB.ui({
+  //       method:'send',
+  //       app_id:'177786062658873',
+  //       link:'https://ncubird.github.io/ncubird/',
+  //       name: 'Send Private Message',
+  //       to:facebook_id
+  //   });
 
-  // if (navigator.userAgent.indexOf("Mobi") > -1){
-  //     window.location.assign("#page-2");
+  if (navigator.userAgent.indexOf("Mobi") > -1){
+      window.location.assign("#page-2");
       
-  // } else {
-  //     window.location.replace("https://www.facebook.com/dialog/send?" + 
-  //                             "app_id=177786062658873" + 
-  //                             "&link=https://ncubird.github.io/ncubird/" + 
-  //                             "&redirect_uri=https://ncubird.github.io/ncubird/"+
-  //                             "&to="+JSON.stringify(facebook_id));
+  } else {
+      window.location.replace("https://www.facebook.com/dialog/send?" + 
+                              "app_id=177786062658873" + 
+                              "&link=https://ncubird.github.io/ncubird/" + 
+                              "&redirect_uri=https://ncubird.github.io/ncubird/"+
+                              "&to="+JSON.stringify(facebook_id));
       
-  // }
+  }
 }
 
 
