295f11fcf6678c10b381363733be4de7bd28ab00
ncubird <ncubird123@gmail.com>
2016-09-27 02:35:13 +0800

    test

--- js/information_block.js
+++ js/information_block.js
@@ -68,11 +68,11 @@ Information_block.prototype.show_block = function(data,facebook_id,post_time,cal
 			}
 
 
-			$('.blockmodal-infomation-contectpassenger').addClass('btn-disable');
+			// $('.blockmodal-infomation-contectpassenger').addClass('btn-disable');
 			if(passenger.length > 0 ){
 				if(facebook_id == $('#facebook_userid').html()){
 					if (navigator.userAgent.indexOf("Mobi") < 0){ //computer
-						$('.blockmodal-infomation-contectpassenger').removeClass('btn-disable');
+						// $('.blockmodal-infomation-contectpassenger').removeClass('btn-disable');
 						$('.blockmodal-infomation-contectpassenger').unbind('click');
 						$('.blockmodal-infomation-contectpassenger').click(function(){
 							var fb_sdk = new Fb_sdk();			
@@ -80,9 +80,11 @@ Information_block.prototype.show_block = function(data,facebook_id,post_time,cal
 							
 						})
 					}else{
+						$('.blockmodal-infomation-contectpassenger').unbind('click');
+						$('.blockmodal-infomation-contectpassenger').click(function(){
 							var fb_sdk = new Fb_sdk();
-							console.log("test");
 							fb_sdk.link_poster(other_message[INFO_UTIL.OTHER_MESSAGE_KEY.FACEBOOK_ID] );
+						}
 					}
 				}
 			}
