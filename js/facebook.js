7654d47e5791ca12de9e753142a521a0be000190
ncubird <ncubird123@gmail.com>
2016-09-26 22:05:19 +0800

    test

--- js/facebook.js
+++ js/facebook.js
@@ -12,10 +12,9 @@ Fb_sdk.prototype.send_message = function(facebook_id){
   //   });
 
   if (navigator.userAgent.indexOf("Mobi") > -1){
-      window.location.assign("#page-2");
-      
+        
   } else {
-      window.location.replace("https://www.facebook.com/dialog/send?" + 
+      window.open("https://www.facebook.com/dialog/send?" + 
                               "app_id=177786062658873" + 
                               "&link=https://ncubird.github.io/ncubird/" + 
                               "&redirect_uri=https://ncubird.github.io/ncubird/"+
